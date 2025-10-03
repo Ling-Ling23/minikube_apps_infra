d45de68eb7f3d262b3b830d0d1c5e0a9e8779b2c
Andrzej Waloszek <waloszek.andrzej@gmail.com>
2025-10-03 15:23:02 +0200

    app image update for live probe

--- docker/app/src/app.py
+++ docker/app/src/app.py
@@ -1,4 +1,5 @@
-import os, socket
+import os, socket, time
+from datetime import datetime
 from flask import Flask, jsonify, send_from_directory, request
 from flask_swagger_ui import get_swaggerui_blueprint
 
@@ -6,6 +7,7 @@ app = Flask(__name__)
 
 #DATA_SET = ['1', '2', '3'] # some data
 
+START_TIME = time.time()
 
 @app.route(f'/static/<path:path>')
 def send_static(path):
